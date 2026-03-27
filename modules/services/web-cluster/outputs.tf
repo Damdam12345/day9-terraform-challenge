0995280bba24fdded9c9f1e9882ce81c4abb698f
Damilare <oadesikemi@gmail.com>
2026-03-27 18:16:08 +0100

    removed asg output

--- modules/services/web-cluster/outputs.tf
+++ modules/services/web-cluster/outputs.tf
@@ -2,8 +2,3 @@
 output "alb_dns_name" {
    value  = aws_lb.webserver.dns_name
 }
-
-# Output the name of the Auto Scaling Group
-output "asg_name" {
-  value       = aws_autoscaling_group.webserver.name
-}
