27c4288c70e2cd96057ea5206271c66853b29ddc
Damilare <oadesikemi@gmail.com>
2026-03-27 19:03:12 +0100

    second version

--- environments/dev/web-cluster/main.tf
+++ environments/dev/web-cluster/main.tf
@@ -27,7 +27,7 @@ data "aws_subnets" "default" {
 }
 
 module "web_cluster" {
-  source        = "../../../modules/services/web-cluster"
+  source        = "github.com/Damdam12345/day9-terraform-challenge//modules/services/web-cluster?ref=v0.0.2"
   cluster_name  = "dev-cluster"
   ami_id        = "ami-09d0c9a85bf1b9ea7"
   instance_type = "t3.micro"
